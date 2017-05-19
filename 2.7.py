d3559ea6ccac363429066c365dcf4a07e9a6dd7e
weirdbrume <weirdbrume@gmail.com>
2017-05-19 21:09:10 +0500

    add 2.7.py

---
+++ 2.7.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d as p3
+import numpy as np
+import time
+import random
+
+from functools import partial
+from numpy.linalg import norm
+
+
+class Perceptron:
+
+    def __init__(self, w, b):
+        """
+        Инициализируем наш объект - перцептрон.
+        w - вектор весов размера (m, 1), где m - количество переменных
+        b - число
+        """
+
+        self.w = w
+        self.b = b
+
+    def forward_pass(self, single_input):
+        """
+        Метод рассчитывает ответ перцептрона при предъявлении одного примера
+        single_input - вектор примера размера (m, 1).
+        Метод возвращает число (0 или 1) или boolean (True/False)
+        """
+
+        result = 0
+        for i in range(0, len(self.w)):
+            result += self.w[i] * single_input[i]
+        result += self.b
+
+        if result > 0:
+            return 1
+        else:
+            return 0
+
+    def vectorized_forward_pass(self, input_matrix):
+        """
+        Метод рассчитывает ответ перцептрона при предъявлении набора примеров
+        input_matrix - матрица примеров размера (n, m), каждая строка - отдельный пример,
+        n - количество примеров, m - количество переменных
+        Возвращает вертикальный вектор размера (n, 1) с ответами перцептрона
+        (элементы вектора - boolean или целые числа (0 или 1))
+        """
+
+        ## Этот метод необходимо реализовать
+        pass
+
+    def train_on_single_example(self, example, y):
+        """
+        принимает вектор активации входов example формы (m, 1)
+        и правильный ответ для него (число 0 или 1 или boolean),
+        обновляет значения весов перцептрона в соответствии с этим примером
+        и возвращает размер ошибки, которая случилась на этом примере до изменения весов (0 или 1)
+        (на её основании мы потом построим интересный график)
+        """
+
+        ## Этот метод необходимо реализовать
+        pass
+
+random.seed(42)  # начальное состояние генератора случайных чисел, чтобы можно было воспроизводить результаты.
+
+data = np.loadtxt('data.csv', delimiter=',')
+pears = data[:, 2] == 1
+apples = np.logical_not(pears)
+plt.scatter(data[apples][:, 0], data[apples][:, 1], color='red')
+plt.scatter(data[pears][:, 0], data[pears][:, 1], color='green')
+plt.xlabel('yellowness')
+plt.ylabel('symmetry')
+plt.show()
