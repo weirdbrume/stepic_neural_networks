e0b0bab4fd5596ff6df2ffc26672321e8994ba82
weirdbrume <weirdbrume@gmail.com>
2017-05-31 21:06:58 +0500

    update 2.7.py

--- 2.7.py
+++ 2.7.py
@@ -9,6 +9,16 @@ from functools import partial
 from numpy.linalg import norm
 
 
+def sigmoid(x):
+    """сигмоидальная функция, работает и с числами, и с векторами (поэлементно)"""
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_prime(x):
+    """производная сигмоидальной функции, работает и с числами, и с векторами (поэлементно)"""
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
 class Perceptron:
 
     def __init__(self, w, b):
@@ -58,19 +68,127 @@ class Perceptron:
         (на её основании мы потом построим интересный график)
         """
 
-        y_ = float(self.vectorized_forward_pass(example.T))
+        y_ = float(self.vectorized_forward_pass(example.T)[0][0])
         dw = y - y_
         self.b += dw
-        self.w += dw
-        return math.fabs(dw)
+        self.w += dw * example
+        return y == y_
 
-random.seed(42)  # начальное состояние генератора случайных чисел, чтобы можно было воспроизводить результаты.
+    def train_until_convergence(self, input_matrix, y, max_steps=1e8):
+        """
+        input_matrix - матрица входов размера (n, m),
+        y - вектор правильных ответов размера (n, 1) (y[i] - правильный ответ на пример input_matrix[i]),
+        max_steps - максимальное количество шагов.
+        Применяем train_on_single_example, пока не перестанем ошибаться или до умопомрачения.
+        Константа max_steps - наше понимание того, что считать умопомрачением.
+        """
+        i = 0
+        errors = 1
+        while errors and i < max_steps:
+            i += 1
+            errors = 0
+            for example, answer in zip(input_matrix, y):
+                example = example.reshape((example.size, 1))
+                error = self.train_on_single_example(example, answer)
+                errors += int(error)  # int(True) = 1, int(False) = 0, так что можно не делать if
 
-data = np.loadtxt('data.csv', delimiter=',')
-pears = data[:, 2] == 1
-apples = np.logical_not(pears)
-plt.scatter(data[apples][:, 0], data[apples][:, 1], color='red')
-plt.scatter(data[pears][:, 0], data[pears][:, 1], color='green')
-plt.xlabel('yellowness')
-plt.ylabel('symmetry')
-plt.show()
+
+class Neuron:
+    def __init__(self, weights, activation_function=sigmoid, activation_function_derivative=sigmoid_prime):
+        """
+        weights - вертикальный вектор весов нейрона формы (m, 1), weights[0][0] - смещение
+        activation_function - активационная функция нейрона, сигмоидальная функция по умолчанию
+        activation_function_derivative - производная активационной функции нейрона
+        """
+
+        assert weights.shape[1] == 1, "Incorrect weight shape"
+
+        self.w = weights
+        self.activation_function = activation_function
+        self.activation_function_derivative = activation_function_derivative
+
+    def forward_pass(self, single_input):
+        """
+        активационная функция логистического нейрона
+        single_input - вектор входов формы (m, 1),
+        первый элемент вектора single_input - единица (если вы хотите учитывать смещение)
+        """
+
+        result = 0
+        for i in range(self.w.size):
+            result += float(self.w[i] * single_input[i])
+        return self.activation_function(result)
+
+    def summatory(self, input_matrix):
+        """
+        Вычисляет результат сумматорной функции для каждого примера из input_matrix.
+        input_matrix - матрица примеров размера (n, m), каждая строка - отдельный пример,
+        n - количество примеров, m - количество переменных.
+        Возвращает вектор значений сумматорной функции размера (n, 1).
+        """
+
+        return input_matrix.dot(self.w)
+
+    def activation(self, summatory_activation):
+        """
+        Вычисляет для каждого примера результат активационной функции,
+        получив на вход вектор значений сумматорной функций
+        summatory_activation - вектор размера (n, 1),
+        где summatory_activation[i] - значение суммматорной функции для i-го примера.
+        Возвращает вектор размера (n, 1), содержащий в i-й строке
+        значение активационной функции для i-го примера.
+        """
+
+        return self.activation_function(summatory_activation)
+
+    def vectorized_forward_pass(self, input_matrix):
+        """
+        Векторизованная активационная функция логистического нейрона.
+        input_matrix - матрица примеров размера (n, m), каждая строка - отдельный пример,
+        n - количество примеров, m - количество переменных.
+        Возвращает вертикальный вектор размера (n, 1) с выходными активациями нейрона
+        (элементы вектора - float)
+        """
+        return self.activation(self.summatory(input_matrix))
+
+    def SGD(self, X, y, batch_size, learning_rate=0.1, eps=1e-6, max_steps=200):
+        """
+        Внешний цикл алгоритма градиентного спуска.
+        X - матрица входных активаций (n, m)
+        y - вектор правильных ответов (n, 1)
+
+        learning_rate - константа скорости обучения
+        batch_size - размер батча, на основании которого
+        рассчитывается градиент и совершается один шаг алгоритма
+
+        eps - критерий остановки номер один: если разница между значением целевой функции
+        до и после обновления весов меньше eps - алгоритм останавливается.
+        Вторым вариантом была бы проверка размера градиента, а не изменение функции,
+        что будет работать лучше - неочевидно. В заданиях используйте первый подход.
+
+        max_steps - критерий остановки номер два: если количество обновлений весов
+        достигло max_steps, то алгоритм останавливается
+
+        Метод возвращает 1, если отработал первый критерий остановки (спуск сошёлся)
+        и 0, если второй (спуск не достиг минимума за отведённое время).
+        """
+
+        # Этот метод необходимо реализовать
+
+        pass
+
+    def update_mini_batch(self, X, y, learning_rate, eps):
+        """
+        X - матрица размера (batch_size, m)
+        y - вектор правильных ответов размера (batch_size, 1)
+        learning_rate - константа скорости обучения
+        eps - критерий остановки номер один: если разница между значением целевой функции
+        до и после обновления весов меньше eps - алгоритм останавливается.
+
+        Рассчитывает градиент (не забывайте использовать подготовленные заранее внешние функции)
+        и обновляет веса нейрона. Если ошибка изменилась меньше, чем на eps - возвращаем 1,
+        иначе возвращаем 0.
+        """
+        # Этот метод необходимо реализовать
+
+        pass
