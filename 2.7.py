28f5fc93c6ce391b8b4147e1f8581f5ea30a54b3
weirdbrume <weirdbrume@gmail.com>
2017-06-07 00:13:39 +0500

    update 2.7.py

--- 2.7.py
+++ 2.7.py
@@ -1,12 +1,4 @@
-import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d as p3
 import numpy as np
-import time
-import random
-import math
-
-from functools import partial
-from numpy.linalg import norm
 
 
 def sigmoid(x):
@@ -17,6 +9,77 @@ def sigmoid(x):
 def sigmoid_prime(x):
     """производная сигмоидальной функции, работает и с числами, и с векторами (поэлементно)"""
     return sigmoid(x) * (1 - sigmoid(x))
+
+
+def J_quadratic(neuron, X, y):
+    """
+    Оценивает значение квадратичной целевой функции.
+    Всё как в лекции, никаких хитростей.
+
+    neuron - нейрон, у которого есть метод vectorized_forward_pass, предсказывающий значения на выборке X
+    X - матрица входных активаций (n, m)
+    y - вектор правильных ответов (n, 1)
+
+    Возвращает значение J (число)
+    """
+
+    assert y.shape[1] == 1, 'Incorrect y shape'
+
+    return 0.5 * np.mean((neuron.vectorized_forward_pass(X) - y) ** 2)
+
+
+def J_quadratic_derivative(y, y_hat):
+    """
+    Вычисляет вектор частных производных целевой функции по каждому из предсказаний.
+    y_hat - вертикальный вектор предсказаний,
+    y - вертикальный вектор правильных ответов,
+
+    В данном случае функция смехотворно простая, но если мы захотим поэкспериментировать
+    с целевыми функциями - полезно вынести эти вычисления в отдельный этап.
+
+    Возвращает вектор значений производной целевой функции для каждого примера отдельно.
+    """
+
+    assert y_hat.shape == y.shape and y_hat.shape[1] == 1, 'Incorrect shapes'
+
+    return (y_hat - y) / len(y)
+
+
+def compute_grad_analytically(neuron, X, y, J_prime=J_quadratic_derivative):
+    """
+    Аналитическая производная целевой функции
+    neuron - объект класса Neuron
+    X - вертикальная матрица входов формы (n, m), на которой считается сумма квадратов отклонений
+    y - правильные ответы для примеров из матрицы X
+    J_prime - функция, считающая производные целевой функции по ответам
+
+    Возвращает вектор размера (m, 1)
+    """
+
+    # Вычисляем активации
+    # z - вектор результатов сумматорной функции нейрона на разных примерах
+
+    z = neuron.summatory(X)
+    y_hat = neuron.activation(z)
+
+    # Вычисляем нужные нам частные производные
+    dy_dyhat = J_prime(y, y_hat)
+    dyhat_dz = neuron.activation_function_derivative(z)
+
+    # осознайте эту строчку:
+    dz_dw = X
+
+    # а главное, эту:
+    grad = ((dy_dyhat * dyhat_dz).T).dot(dz_dw)
+
+    # можно было написать в два этапа. Осознайте, почему получается одно и то же
+    # grad_matrix = dy_dyhat * dyhat_dz * dz_dw
+    # grad = np.sum(, axis=0)
+
+    # Сделаем из горизонтального вектора вертикальный
+    grad = grad.T
+
+    return grad
 
 
 class Perceptron:
@@ -151,6 +214,27 @@ class Neuron:
         """
         return self.activation(self.summatory(input_matrix))
 
+    def update_mini_batch(self, X, y, learning_rate, eps):
+        """
+        X - матрица размера (batch_size, m)
+        y - вектор правильных ответов размера (batch_size, 1)
+        learning_rate - константа скорости обучения
+        eps - критерий остановки номер один: если разница между значением целевой функции
+        до и после обновления весов меньше eps - алгоритм останавливается.
+
+        Рассчитывает градиент (не забывайте использовать подготовленные заранее внешние функции)
+        и обновляет веса нейрона. Если ошибка изменилась меньше, чем на eps - возвращаем 1,
+        иначе возвращаем 0.
+        """
+
+        J_before_weights_update = J_quadratic(self, X, y)
+        grad = compute_grad_analytically(self, X, y)
+        self.w = self.w - learning_rate * grad
+        J_after_weights_update = J_quadratic(self, X, y)
+        dJ = abs(J_before_weights_update - J_after_weights_update)
+
+        return int(dJ < eps)
+
     def SGD(self, X, y, batch_size, learning_rate=0.1, eps=1e-6, max_steps=200):
         """
         Внешний цикл алгоритма градиентного спуска.
@@ -173,46 +257,87 @@ class Neuron:
         и 0, если второй (спуск не достиг минимума за отведённое время).
         """
 
-        # Этот метод необходимо реализовать
+        res = 0
+        step = 0
+        while res != 1 and step < max_steps:
+            mini_batch = np.random.choice(len(X), batch_size, replace=False)
+            res = self.update_mini_batch(X[mini_batch], y[mini_batch], learning_rate, eps)
+            step += 1
 
-        pass
+        return res
 
-    def update_mini_batch(self, X, y, learning_rate, eps):
+    def compute_grad_numerically(self, X, y, J=J_quadratic, eps=10e-2):
         """
-        X - матрица размера (batch_size, m)
-        y - вектор правильных ответов размера (batch_size, 1)
-        learning_rate - константа скорости обучения
-        eps - критерий остановки номер один: если разница между значением целевой функции
-        до и после обновления весов меньше eps - алгоритм останавливается.
-
-        Рассчитывает градиент (не забывайте использовать подготовленные заранее внешние функции)
-        и обновляет веса нейрона. Если ошибка изменилась меньше, чем на eps - возвращаем 1,
-        иначе возвращаем 0.
+        Численная производная целевой функции
+        neuron - объект класса Neuron
+        X - вертикальная матрица входов формы (n, m), на которой считается сумма квадратов отклонений
+        y - правильные ответы для тестовой выборки X
+        J - целевая функция, градиент которой мы хотим получить
+        eps - размер $\delta w$ (малого изменения весов)
         """
 
-        # считаем y^
-        z = self.summatory(X)
-        y_ = self.activation(z)
+        initial_cost = J(self, X, y)
+        w_0 = self.w
+        num_grad = np.zeros(w_0.shape)
 
-        # считаем градиент
-        dy_dy_ = (y_ - y) / len(y)
-        dy__dz = self.activation_function_derivative(z)
-        dz_dw = X
-        grad = ((dy_dy_ * dy__dz).T.dot(dz_dw)).T
+        for i in range(len(w_0)):
+            old_wi = self.w[i].copy()
+            # Меняем вес
+            self.w[i] += eps
 
-        # считаем целевую ф-ию до обновления весов
-        J_before_weights_update = 0.5 * np.mean((self.vectorized_forward_pass(X) - y) ** 2)
+            # Считаем новое значение целевой функции и вычисляем приближенное значение градиента
+            num_grad[i] = (J(self, X, y) - initial_cost) / eps
 
-        # обновляем веса нейрона
-        self.w = self.w - learning_rate * grad
+            # Возвращаем вес обратно. Лучше так, чем -= eps, чтобы не накапливать ошибки округления
+            self.w[i] = old_wi
 
-        # считаем целевую ф-ию после обновления весов
-        J_after_weights_update = 0.5 * np.mean((self.vectorized_forward_pass(X) - y) ** 2)
+        # проверим, что не испортили нейрону веса своими манипуляциями
+        assert np.allclose(self.w, w_0), "МЫ ИСПОРТИЛИ НЕЙРОНУ ВЕСА"
 
-        # находим разность значений целевой ф-ии
-        dJ = J_before_weights_update - J_after_weights_update
+        return num_grad
 
-        return int(dJ < eps)
+    def compute_grad_numerically_2(self, X, y, J=J_quadratic, eps=10e-2):
+        """
+        Численная производная целевой функции.
+        neuron - объект класса Neuron с вертикальным вектором весов w,
+        X - вертикальная матрица входов формы (n, m), на которой считается сумма квадратов отклонений,
+        y - правильные ответы для тестовой выборки X,
+        J - целевая функция, градиент которой мы хотим получить,
+        eps - размер $\delta w$ (малого изменения весов).
+        """
+
+        w_0 = self.w
+        num_grad = np.zeros(w_0.shape)
+
+        for i in range(len(w_0)):
+            old_wi = self.w[i].copy()
+            # Меняем вес
+            self.w[i] += eps
+
+            # Считаем новое значение целевой функции f(x+dx)
+            pos_cost = J(self, X, y)
+
+            # Возвращаем вес обратно. Лучше так, чем -= eps, чтобы не накапливать ошибки округления
+            self.w[i] = old_wi
+
+            # снова меняем вес
+            self.w[i] -= eps
+
+            # Считаем новое значение целевой функции f(x-dx)
+            neg_cost = J(self, X, y)
+
+            # вычисляем приближенное значение градиента f(x+dx)-f(x-dx)/2dx
+            num_grad[i] = (pos_cost - neg_cost) / (2 * eps)
+
+            # Возвращаем вес обратно
+            self.w[i] = old_wi
+
+
+        # проверим, что не испортили нейрону веса своими манипуляциями
+        assert np.allclose(self.w, w_0), "МЫ ИСПОРТИЛИ НЕЙРОНУ ВЕСА"
+
+        return num_grad
+
 
 np.random.seed(42)
 n = 10
