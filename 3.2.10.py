a12ffaf242b6c45ed83084cdfc88c6b58601bb84
weirdbrume <weirdbrume@gmail.com>
2018-01-24 19:18:27 +0500

    add 3.2.10.py

---
+++ 3.2.10.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def sigmoid(x):
+    """сигмоидальная функция, работает и с числами, и с векторами (поэлементно)"""
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_prime(x):
+    """производная сигмоидальной функции, работает и с числами, и с векторами (поэлементно)"""
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
+def get_error(deltas, sums, weights):
+    """
+    compute error on the previous layer of network
+    deltas - ndarray of shape (n, n_{l+1})
+    sums - ndarray of shape (n, n_l)
+    weights - ndarray of shape (n_{l+1}, n_l)
+    """
+
+    return np.mean(deltas.dot(weights) * sigmoid_prime(sums), 0)
