1c16cbc3a9113eab4c3129e06bf040e60ded0327
weirdbrume <weirdbrume@gmail.com>
2018-01-28 21:19:38 +0500

    add 4.4.2.py

---
+++ 4.4.2.py
@@ -0,0 +1,203 @@
+import numpy as np
+import random
+import doctest
+
+
+def sigmoid(z):
+    """The sigmoid function.
+    Сигмоида
+    """
+    return 1.0/(1.0+np.exp(-z))
+
+
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function.
+    Производная сигмоиды по e (шутка). По x
+    """
+    return sigmoid(z)*(1-sigmoid(z))
+
+
+def cost_function(network, test_data, onehot=True):
+    c = 0
+    for example, y in test_data:
+        if not onehot:
+            y = np.eye(3, 1, k=-int(y))
+        yhat = network.feedforward(example)
+        c += np.sum((y - yhat)**2)
+    return c / len(test_data)
+
+
+def sample_backprop1():
+    """
+    Пример тестового случая. Создает сеть, запускает функцию backprop и
+    проверяет возвращенные значения.
+
+    >>> nabla_b, nabla_w = sample_backprop1()
+    >>> print(nabla_b[0])
+    [[ 0.00214254]
+     [-0.05287709]]
+    >>> print(nabla_w[0])
+    [[ 0.00214254  0.00428509  0.00642763]
+     [-0.05287709 -0.10575419 -0.15863128]]
+    """
+    nn = Network([3, 2])
+    nn.biases = [np.array([[-1], [-1]])]
+    nn.weights = [np.array([[-1, 1, -1], [1, -1, 1]])]
+    x = np.array([[1], [2], [3]])
+    y = np.array([[0], [1]])
+
+    return nn.backprop(x, y)
+
+
+class Network:
+
+    def __init__(self, sizes, output=True):
+        """
+        Список ``sizes`` содержит количество нейронов в соответствующих слоях
+        нейронной сети. К примеру, если бы этот лист выглядел как [2, 3, 1],
+        то мы бы получили трёхслойную нейросеть, с двумя нейронами в первом
+        (входном), тремя нейронами во втором (промежуточном) и одним нейроном
+        в третьем (выходном, внешнем) слое. Смещения и веса для нейронных сетей
+        инициализируются случайными значениями, подчиняющимися стандартному нормальному
+        распределению. Обратите внимание, что первый слой подразумевается слоем,
+        принимающим входные данные, поэтому мы не будем добавлять к нему смещение
+        (делать это не принято, поскольку смещения используются только при
+        вычислении выходных значений нейронов последующих слоёв)
+        """
+
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x)
+                        for x, y in zip(sizes[:-1], sizes[1:])]
+        self.output = output
+
+    def feedforward(self, a):
+        """
+        Вычислить и вернуть выходную активацию нейронной сети
+        при получении ``a`` на входе (бывшее forward_pass).
+        """
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a) + b)
+        return a
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta,
+            test_data=None):
+        """
+        Обучить нейронную сеть, используя алгоритм стохастического
+        (mini-batch) градиентного спуска.
+        ``training_data`` - лист кортежей вида ``(x, y)``, где
+        x - вход обучающего примера, y - желаемый выход (в формате one-hot).
+        Роль остальных обязательных параметров должна быть понятна из их названия.
+        Если предоставлен опциональный аргумент ``test_data``,
+        то после каждой эпохи обучения сеть будет протестирована на этих данных
+        и промежуточный результат обучения будет выведен в консоль.
+        ``test_data`` -- это список кортежей из входных данных
+        и номеров правильных классов примеров (т.е. argmax(y),
+        если y -- набор ответов в той же форме, что и в тренировочных данных).
+        Тестирование полезно для мониторинга процесса обучения,
+        но может существенно замедлить работу программы.
+        """
+
+        if test_data is not None: n_test = len(test_data)
+        n = len(training_data)
+        success_tests = 0
+        for j in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k + mini_batch_size]
+                for k in range(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data is not None and self.output:
+                success_tests = self.evaluate(test_data)
+                print("Эпоха {0}: {1} / {2}".format(
+                    j, success_tests, n_test))
+            elif self.output:
+                print("Эпоха {0} завершена".format(j))
+        if test_data is not None:
+            return success_tests / n_test
+
+    def update_mini_batch(self, mini_batch, eta):
+        """
+        Обновить веса и смещения нейронной сети, сделав шаг градиентного
+        спуска на основе алгоритма обратного распространения ошибки, примененного
+        к одному mini batch.
+        ``mini_batch`` - список кортежей вида ``(x, y)``,
+        ``eta`` - величина шага (learning rate).
+        """
+
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+
+        eps = eta / len(mini_batch)
+        self.weights = [w - eps * nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b - eps * nb for b, nb in zip(self.biases, nabla_b)]
+
+    def backprop(self, x, y):
+        """
+        Возвращает кортеж ``(nabla_b, nabla_w)`` -- градиент целевой функции по всем параметрам сети.
+        ``nabla_b`` и ``nabla_w`` -- послойные списки массивов ndarray,
+        такие же, как self.biases и self.weights соответственно.
+        """
+
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        # прямое распространение (forward pass)
+        activation = x
+        activations = [x] # лист, хранящий все активации слой за слоем
+        zs = []  # лист, хранящий все z векторы (значения сумматорной функции) слой за слоем
+        for b, w in zip(self.biases, self.weights):
+            z = w.dot(activation) + b
+            zs.append(z)
+            # считаем активации
+            activation = sigmoid(z)
+            activations.append(activation)
+
+        # обратное распространение (backward pass)
+        delta = (activations[-1] - y) * sigmoid_prime(zs[-1])  # ошибка выходного слоя
+        nabla_b[-1] = delta  # производная J по смещениям выходного слоя
+        nabla_w[-1] = delta.dot(activations[-2].T)  # производная J по весам выходного слоя
+
+        # Обратите внимание, что переменная l в цикле ниже используется
+        # немного иначе, чем в лекциях.  Здесь l = 1 означает последний слой,
+        # l = 2 - предпоследний и так далее.
+        # Мы перенумеровали схему, чтобы с удобством для себя
+        # использовать тот факт, что в Python к переменной типа list
+        # можно обращаться по негативному индексу.
+        for l in range(2, self.num_layers):
+            # дополнительные вычисления, чтобы легче записывалось
+            delta = self.weights[-l+1].T.dot(delta) * sigmoid_prime(zs[-l])  # ошибка на слое L-l
+            nabla_b[-l] = delta  # производная J по смещениям L-l-го слоя
+            nabla_w[-l] = delta.dot(activations[-l-1].T)  # производная J по весам L-l-го слоя
+        return nabla_b, nabla_w
+
+    def evaluate(self, test_data):
+        """
+        Вернуть количество тестовых примеров, для которых нейронная сеть
+        возвращает правильный ответ. Обратите внимание: подразумевается,
+        что выход нейронной сети - это индекс, указывающий, какой из нейронов
+        последнего слоя имеет наибольшую активацию.
+        """
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        """
+        Возвращает вектор частных производных (\partial C_x) / (\partial a)
+        целевой функции по активациям выходного слоя.
+        """
+        return (output_activations - y)
+
+
+# Непосредственный запуск теста.
+# Указывается имя объекта (функции, класса и т. п.), из описания которой берутся тесты.
+# Обычно аргумент verbose=True не используют, и если всё хорошо,
+# то система просто ничего не выводит. Мы оставили его для наглядности.
+doctest.run_docstring_examples(sample_backprop1, globals(), verbose=True)
